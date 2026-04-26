8c0d508ae65ecfdfc4398edfb0fdf6665379cc36
mp1711 <136905595+mp1711@users.noreply.github.com>
2026-04-26 18:58:17 +0530

    Add: cheapest-flight-path - submission-7

---
+++ Data Structures & Algorithms/cheapest-flight-path/submission-7.py
@@ -0,0 +1,28 @@
+from collections import defaultdict
+from heapq import heappush, heappop
+class Solution:
+    def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
+
+        graph = defaultdict(list)
+
+        for u, v, w in flights: 
+            graph[u].append((v, w))
+        
+
+        heap = [(0, 0, src)]
+        visited = set()
+
+        while heap: 
+            price, stops, node = heappop(heap)
+            if node==2: 
+                print(heap)
+            if node==dst: 
+                return price
+            visited.add(node)
+            for nei, wt in graph[node]: 
+                if stops<=k: 
+                    heappush(heap, (wt+price, stops+1, nei))
+        
+        return -1
+            
+        
