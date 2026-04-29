b8081267f0a368796fd5455b2479b3cb51a7b5e1
mp1711 <136905595+mp1711@users.noreply.github.com>
2026-04-30 00:30:45 +0530

    Add: bus-routes - submission-0

---
+++ Data Structures & Algorithms/bus-routes/submission-0.py
@@ -0,0 +1,36 @@
+from collections import defaultdict, deque
+class Solution:
+    def numBusesToDestination(self, routes: List[List[int]], source: int, target: int) -> int:
+        
+        if source==target: 
+            return 0
+        
+        stop_bus = defaultdict(list)
+        
+        for bus, route in enumerate(routes): 
+            for stop in route: 
+                stop_bus[stop].append(bus)
+
+        q = deque([(source, 0)])
+        vis_bus = set()
+        vis_stop = {source}
+
+        while q: 
+            stop, cnt = q.popleft()
+
+            if stop==target: 
+                return cnt
+
+            for bus in stop_bus[stop]: 
+                if bus not in vis_bus: 
+                    vis_bus.add(bus)
+                    for new_stop in routes[bus]: 
+                        if new_stop not in vis_stop: 
+                            vis_stop.add(new_stop)
+                            q.append((new_stop, cnt+1))
+        
+        return -1
+
+
+
+
