5580a903b6a7dd1b92cbd39c8fcb34f4ee3c915e
Mukesh Prasanna <136905595+mp1711@users.noreply.github.com>
2026-08-19 23:17:07 +0530

    Add: foreign-dictionary - submission-4

---
+++ Data Structures & Algorithms/foreign-dictionary/submission-4.py
@@ -0,0 +1,48 @@
+class Solution:
+    def foreignDictionary(self, words: List[str]) -> str:
+
+        chars = set()
+        for word in words: 
+            for ch in word: 
+                chars.add(ch)
+        
+        graph = {i:[] for i in chars}
+        indeg = {i:0 for i in chars}
+        
+        for i in range(1, len(words)): 
+            w1 = words[i-1]
+            w2 = words[i]
+
+            if len(w1)>len(w2) and w1[:len(w2)]==w2: 
+                return ""
+            
+            for j in range(len(w1)): 
+                if w1[j]!=w2[j]: 
+                    indeg[w2[j]]+=1
+                    graph[w1[j]].append(w2[j])
+                    break
+        
+        q = deque()
+        for i in indeg: 
+            if not indeg[i]: 
+                q.append(i)
+        
+        vis = set()
+        res = []
+        while q: 
+            ch = q.popleft()
+            if ch in vis: 
+                continue
+            vis.add(ch)
+            res.append(ch)
+            for nei in graph[ch]: 
+                indeg[nei]-=1
+                if indeg[nei]==0: 
+                    q.append(nei)
+        
+        return "".join(res) if len(res)==len(chars) else ""
+
+
+
+
+        
